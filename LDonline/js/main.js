fd8b4b84fed9d13084a322ca1515780ce811df53
c2h4 <1079734644@qq.com>
2018-12-31 16:48:38 +0800

    12.31

---
+++ LDonline/js/main.js
@@ -0,0 +1,8 @@
+require('./demo.js');
+require('./imagesloaded.pkgd.min.js');
+require('./jquery-1.8.2.min.js');
+require('./jquery.mousewheel.js');
+require('./modernizr-1.5.min.js');
+require('./scripts.js');
+require('./TweenMax.min.js');
+require('./video.js');
